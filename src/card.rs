52c5f9b874c2614f337be57584afa035c3257e01
Tyler Gwin <s3946514@student.rmit.edu.au>
2024-04-28 17:48:15 +1000

    Card type is now an enum of either Joker or a Suited card type. Also impleneted to display functions for Card and Suit.

--- src/card.rs
+++ src/card.rs
@@ -1,13 +1,35 @@
-// Represents a single card
-struct Card {
-    rank: usize,
-    suit: Suit,
-}
-
+// Suit is a bitfield enum (repr(u8) tells the compiler to use a u8 to store the enum)
 #[repr(u8)]
 enum Suit {
     Spades = 0b0001,
     Hearts = 0b0010,
     Diamonds = 0b0100,
     Clubs = 0b1000,
+}
+
+// A card could be a suited card or a joker
+enum Card {
+    Suited { rank: usize, suit: Suit },
+    Joker,
+}
+
+// Overriding the Display trait for Card and Suit
+impl fmt::Display for Card {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Card::Suited { rank, suit } => write!(f, "{} of {:?}", rank, suit),
+            Card::Joker => write!(f, "Joker"),
+        }
+    }
+}
+
+impl fmt::Display for Suit {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Suit::Spades => write!(f, "Spades"),
+            Suit::Hearts => write!(f, "Hearts"),
+            Suit::Diamonds => write!(f, "Diamonds"),
+            Suit::Clubs => write!(f, "Clubs"),
+        }
+    }
 }
